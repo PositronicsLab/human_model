a063d27a5016deb8a51dd40dd0d6ec3ec957c158
Joshua Lurz <jlurz24@gmail.com>
2015-01-04 14:36:05 -0500

    Add skeleton for PD controller. Fix compliler errors in human_pacer

--- human_pacer.cpp
+++ human_pacer.cpp
@@ -85,18 +85,18 @@ int main(){
 
   // get a Jacobian for a particular point on a humanoid link
   // 1. I'll specify a random link
-  Moby::RCArticulatedBody body = robot->get_articulated_body();
+  Moby::RCArticulatedBodyPtr body = robot->get_articulated_body();
   std::vector<Moby::RigidBodyPtr> links = body->get_links();
   std::random_shuffle(links.begin(), links.end());
   Moby::RigidBodyPtr link = links.front();
 
   // 2. Now we'll pick a frame defined at (1,0,0) w.r.t. the link
-  shared_ptr<Ravelin::Pose3d> P;
+  boost::shared_ptr<Ravelin::Pose3d> P;
   P->x = Ravelin::Origin3d(1.0, 0.0, 0.0);
   P->rpose = link->get_pose();
 
   // 2a. This is a verification step (described in email)
-  Transform3d gTP = Pose3d::calc_relative_pose(P, GLOBAL);
+  Transform3d gTP = Pose3d::calc_relative_pose(P, Moby::GLOBAL);
   std::cout << "pose of bilateral constraint frame: " << std::endl << gTP;
 
   // 3. Finally we get the Jacobian of the humanoid (R) 
