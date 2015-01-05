167eba4b8dc268cd82ce9e96ef68efdaa35c03c5
Joshua Lurz <jlurz24@gmail.com>
2015-01-04 22:22:25 -0500

    Working PR2 that attempts to hold up the human

--- stable-controller-plugin.cpp
+++ stable-controller-plugin.cpp
@@ -8,8 +8,7 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <stdio.h>
-#include "pd_controller.h"
-#include <gazebo/physics/JointController.hh>
+#include <gazebo/common/PID.hh>
 
 using namespace std;
 using namespace gazebo::physics;
@@ -17,8 +16,6 @@ using namespace gazebo::physics;
 void init_plugin(){
 }
 
-static const double KP = 1;
-static const double KD = 0.1;
 static const string TARGET_JOINT_NAME = "r_shoulder_lift_joint";
 static const unsigned int JOINT_AXIS_INDEX = 0;
 
@@ -27,9 +24,11 @@ namespace gazebo {
    {
      private: physics::ModelPtr model;
      private: event::ConnectionPtr connection;
-     private: boost::shared_ptr<PDController> pd;
+     private: boost::shared_ptr<common::PID> jointPID;
      private: math::Angle targetAngle;
      private: double totalEffort;
+     private: physics::JointPtr joint;
+     private: common::Time prevUpdateTime;
 
      public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
      {
@@ -39,26 +38,39 @@ namespace gazebo {
        totalEffort = 0.0;
        
        connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&ControllerPlugin::updateController, this));
-
-       targetAngle = this->model->GetJoint(TARGET_JOINT_NAME)->GetAngle(JOINT_AXIS_INDEX);
+       joint = this->model->GetJoint(TARGET_JOINT_NAME);
+       targetAngle = joint->GetAngle(JOINT_AXIS_INDEX);
        cout << "Target angle: " << *targetAngle << endl;
-       JointControllerPtr jc = model->GetJointController();
-       jc->SetJointPosition(TARGET_JOINT_NAME, 0.0, JOINT_AXIS_INDEX);
-       pd = boost::shared_ptr<PDController>(new PDController(TARGET_JOINT_NAME, model->GetWorld()->GetSimTime().Double(), KD, KP));
+       jointPID = boost::shared_ptr<common::PID>(new common::PID(1000000.0, 0.0, 1.0, 10)); /* TODO: Determine if we should turn limits on */
      }
     
+
+     // Inspired by: http://answers.gazebosim.org/question/2341/set-and-get-position-of-gazebo-model-using-ros/
      private: void updateController(){
        cout << "Starting plugin update" << endl;
-       common::Time currTime = this->model->GetWorld()->GetSimTime();
 
-       physics::JointPtr joint = this->model->GetJoint(TARGET_JOINT_NAME);
-       math::Angle currAngle = joint->GetAngle(JOINT_AXIS_INDEX);
-       cout << "Current angle: " << currAngle << endl;
-       double effort = -1.0 * pd->update(*currAngle, *targetAngle, currTime.Double());
-       totalEffort += effort;
-       cout << "Current Effort: " << joint->GetForce(JOINT_AXIS_INDEX) << " Deleta Effort: " << effort << " Total Effort: " << totalEffort << endl;
-       // Max force is 30
-       // joint->SetForce(JOINT_AXIS_INDEX, totalEffort);
+       // compute the steptime for the PID
+       common::Time currTime = this->model->GetWorld()->GetSimTime();
+       common::Time stepTime = currTime - this->prevUpdateTime;
+       this->prevUpdateTime = currTime;
+
+       // set the current position of the joint, and the target position, 
+       // and the maximum effort limit
+       double posTarget = *targetAngle;
+       double posCurr = this->joint->GetAngle(JOINT_AXIS_INDEX).Radian();
+       double maxCmd = this->joint->GetEffortLimit(JOINT_AXIS_INDEX);
+       cout << "Max effort: " << maxCmd << endl;
+
+       // calculate the error between the current position and the target one
+       double posErr = posCurr - posTarget;
+       cout << "Pos curr: " << posCurr << " Pos target: " << posTarget << " posErr: " << posErr << endl;
+
+       // compute the effort via the PID, which you will apply on the joint
+       double effortCmd = this->jointPID->Update(posErr, stepTime);
+
+       cout << "Executing effort: " << effortCmd << endl;
+       // apply the force on the joint
+       this->joint->SetForce(JOINT_AXIS_INDEX, effortCmd);       
        cout << "Ending plugin update" << endl; 
    }
      };
