7da5a98a4a07c26e04b40e4255b6fa91e7e28e24
Joshua Lurz <jlurz24@gmail.com>
2015-01-24 13:46:49 -0500

    Get stable controlled plugin working for all right arm joints

--- stable-controller-plugin.cpp
+++ stable-controller-plugin.cpp
@@ -13,67 +13,88 @@
 using namespace std;
 using namespace gazebo::physics;
 
-void init_plugin(){
-}
-
-static const string TARGET_JOINT_NAME = "r_shoulder_lift_joint";
-static const unsigned int JOINT_AXIS_INDEX = 0;
-
 namespace gazebo {
    class ControllerPlugin : public ModelPlugin
    {
      private: physics::ModelPtr model;
      private: event::ConnectionPtr connection;
-     private: boost::shared_ptr<common::PID> jointPID;
-     private: math::Angle targetAngle;
-     private: double totalEffort;
-     private: physics::JointPtr joint;
+
+     // One per joint
+     private: std::vector<boost::shared_ptr<common::PID> > jointPIDs;
+     private: std::vector<math::Angle> targetAngles;
+     private: std::vector<double> totalEfforts;
+     private: std::vector<physics::JointPtr> joints;
+     
      private: common::Time prevUpdateTime;
 
-     public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+     public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
      {
-       cout << "Initializing plugin" << endl;
+       cout << "Initializing plugin stable controller plugin" << endl;
+       
+       // Iterate over the element children to find the joints to control
+       if(!_sdf->HasElement("controlled-joint")){
+         cerr << "Controlled joint not specified" << endl;
+         return;
+       }
 
+       for(sdf::ElementPtr child = _sdf->GetElement("controlled-joint"); child != NULL; child = child->GetNextElement()){
+         if(child->GetName() == "controlled-joint"){
+           const std::string jointName = child->GetValueString();
+           cout << "Adding controlled joint: " << jointName << endl;
+           const physics::JointPtr joint = _parent->GetJoint(jointName);
+
+           if(joint == NULL){
+             cerr << "Could not find joint: " << jointName << endl;
+             continue;
+           }
+
+           joints.push_back(joint);
+           for(unsigned int j = 0; j < joint->GetAngleCount(); ++j){
+             targetAngles.push_back(joint->GetAngle(j));
+             jointPIDs.push_back(boost::shared_ptr<common::PID>(new common::PID(2.0, 0.0, 1.0, 10)));
+           }
+         }    
+       }
+       totalEfforts.resize(jointPIDs.size());
        this->model = _parent;
-       totalEffort = 0.0;
        
        connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&ControllerPlugin::updateController, this));
-       joint = this->model->GetJoint(TARGET_JOINT_NAME);
-       targetAngle = joint->GetAngle(JOINT_AXIS_INDEX);
-       cout << "Target angle: " << *targetAngle << endl;
-       jointPID = boost::shared_ptr<common::PID>(new common::PID(1000000.0, 0.0, 1.0, 10)); /* TODO: Determine if we should turn limits on */
      }
     
 
      // Inspired by: http://answers.gazebosim.org/question/2341/set-and-get-position-of-gazebo-model-using-ros/
      private: void updateController(){
-       cout << "Starting plugin update" << endl;
 
        // compute the steptime for the PID
        common::Time currTime = this->model->GetWorld()->GetSimTime();
        common::Time stepTime = currTime - this->prevUpdateTime;
        this->prevUpdateTime = currTime;
 
-       // set the current position of the joint, and the target position, 
-       // and the maximum effort limit
-       double posTarget = *targetAngle;
-       double posCurr = this->joint->GetAngle(JOINT_AXIS_INDEX).Radian();
-       double maxCmd = this->joint->GetEffortLimit(JOINT_AXIS_INDEX);
-       cout << "Max effort: " << maxCmd << endl;
+       int index = 0;
+       for(unsigned int i = 0; i < joints.size(); ++i){
+         const physics::JointPtr joint = joints[i];
+         for(unsigned int j = 0; j < joint->GetAngleCount(); ++j){
+           // get the current position of the joint and the target position, 
+           double posTarget = *targetAngles[index];
+           double posCurr = joint->GetAngle(j).Radian();
 
-       // calculate the error between the current position and the target one
-       double posErr = posCurr - posTarget;
-       cout << "Pos curr: " << posCurr << " Pos target: " << posTarget << " posErr: " << posErr << endl;
+          // calculate the error between the current position and the target one
+          double posErr = posCurr - posTarget;
 
-       // compute the effort via the PID, which you will apply on the joint
-       double effortCmd = this->jointPID->Update(posErr, stepTime);
+          // compute the effort via the PID, which you will apply on the joint
+          totalEfforts[index] += jointPIDs[index]->Update(posErr, stepTime);
 
-       cout << "Executing effort: " << effortCmd << endl;
-       // apply the force on the joint
-       this->joint->SetForce(JOINT_AXIS_INDEX, effortCmd);       
-       cout << "Ending plugin update" << endl; 
+          // apply the force on the joint
+          joints[i]->SetForce(j, totalEfforts[index]);
+          index++;
+       }
+     }     
    }
-     };
+   ~ControllerPlugin(){
+     cout << "Destroying stable joint controller plugin" << endl;
+     event::Events::DisconnectWorldUpdateBegin(this->connection);
+   }
+  };
 
    // Register this plugin with the simulator
    GZ_REGISTER_MODEL_PLUGIN(ControllerPlugin)
