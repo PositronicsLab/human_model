a8cd613d53077548d122cd9d12df369ed6797424
Joshua Lurz <jlurz24@gmail.com>
2015-01-28 21:36:07 -0500

    Get FK working

--- src/initial-joint-position-plugin.cpp
+++ src/initial-joint-position-plugin.cpp
@@ -14,6 +14,7 @@
 #include <kdl/chainfksolver.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/frames_io.hpp>
+#include <boost/math/constants/constants.hpp>
 
 using namespace std;
 using namespace KDL;
@@ -21,6 +22,11 @@ using namespace KDL;
 #define USE_FIXED_SEED 1
 #define PRINT_POSITIONS 0
 #define PRINT_DEBUG 1
+
+template <class T>
+bool rough_eq(T lhs, T rhs, T epsilon = 0.001){
+  return fabs(lhs - rhs) < epsilon;
+}
 
 namespace gazebo {
 
@@ -73,7 +79,31 @@ namespace gazebo {
       assert(false);
     }
     
-    private: Chain constructChain(physics::JointPtr root, physics::LinkPtr endEffector, vector<double>& q){
+    private: vector<double> jointAngles(physics::JointPtr root, physics::LinkPtr endEffector) const {
+        assert(root != nullptr && "Root joint is null");
+        assert(endEffector != nullptr && "End effector is null");
+        
+        vector<double> q;
+        physics::JointPtr parent = root;
+        physics::LinkPtr link = nullptr;
+        do {
+          link = parent->GetChild();
+            
+          // Iterate over degrees of freedom
+          for(unsigned int i = 0; i < parent->GetAngleCount(); ++i){
+            q.push_back(parent->GetAngle(i).Radian());
+          }
+          
+          assert(link->GetChildJoints().size() == 1 || (link->GetChildJoints().size() == 0 && link == endEffector));
+          
+          if(link->GetChildJoints().size() > 0){
+            parent = link->GetChildJoints()[0];
+          }
+        } while (link != endEffector);
+        return q;
+    }
+    
+    private: Chain constructChain(physics::JointPtr root, physics::LinkPtr endEffector){
         assert(root != nullptr && "Root joint is null");
         assert(endEffector != nullptr && "End effector is null");
         
@@ -82,25 +112,16 @@ namespace gazebo {
         physics::JointPtr parent = root;
         physics::LinkPtr link = nullptr;
         do {
-          cout << "Operating on joint: " << parent->GetName() << endl;
           link = parent->GetChild();
             
           // Iterate over degrees of freedom
           for(unsigned int i = 0; i < parent->GetAngleCount(); ++i){
             Vector axis = Vector(parent->GetLocalAxis(i).x, parent->GetLocalAxis(i).y, parent->GetLocalAxis(i).z);
-            math::Vector3 anchor = parent->GetAnchor(i);
-            Vector origin = (parent == root && i == 0) ? Vector(anchor.x, anchor.y, anchor.z) : Vector();
-            cout << "Origin: " << origin << endl;
-            
-            Joint kdlJoint = Joint(parent->GetName() + axisName(jointType(parent, i)), origin, axis, Joint::RotAxis);
-            
-            cout << "Joint angle: " << parent->GetAngle(i).Radian() << endl;
-            q.push_back(parent->GetAngle(i).Radian());
+            Joint kdlJoint = Joint(parent->GetName() + axisName(jointType(parent, i)), Vector(), axis, Joint::RotAxis);
             
             // Construct a segment.
             bool isFinalAxis = i == parent->GetAngleCount() - 1;
             Frame linkLength = Frame(Vector(isFinalAxis ? link->GetBoundingBox().GetZLength() : 0.0, 0.0, 0.0));
-            cout << "Link length: " << linkLength.p << endl;
             Segment segment = Segment(parent->GetName() + axisName(jointType(parent, i)), kdlJoint, linkLength);
             chain.addSegment(segment);
           }
@@ -113,6 +134,55 @@ namespace gazebo {
         } while (link != endEffector);
         
         return chain;
+    }
+    
+    private: bool checkFK(const Chain& chain, physics::LinkPtr leaf, physics::JointPtr root) const {
+      // Get the current angles of all the joints
+      vector<double> q = jointAngles(root, leaf);
+        
+      // Construct the solver
+      ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(chain);
+      
+      // Create joint array
+      unsigned int nj = chain.getNrOfJoints();
+      assert(nj == q.size());
+      
+      KDL::JntArray jointPositions = JntArray(nj);
+      for(unsigned int i = 0; i < nj; i++){
+        jointPositions(i) = q[i];
+      }
+
+      // Create the frame that will contain the results
+      KDL::Frame cartPos;
+      
+      // Calculate forward position kinematics
+      // Note: FK is in local coordinates
+      bool status = fksolver.JntToCart(jointPositions, cartPos);
+      if(status >= 0){
+        // Translate to the global frame.
+        math::Vector3 origin = root->GetAnchor(0);
+        Frame baseFrame = Frame(Rotation::RotY(-boost::math::constants::pi<double>() / 2.0), Vector(origin.x, origin.y, origin.z));
+        
+        Vector pos = (baseFrame * cartPos).p;
+        pos = Vector(pos.x(), pos.y(), pos.z() - leaf->GetBoundingBox().GetZLength() / 2.0);
+        
+        math::Vector3 endPos = leaf->GetBoundingBox().GetCenter();
+        if(!rough_eq(pos.x(), endPos.x)){
+          cout << "X values did not match: " << pos << " " << endPos << endl;
+          return false;
+        }
+        if(!rough_eq(pos.y(), endPos.y)){
+          cout << "Y values did not match: " << pos << " " << endPos << endl;
+          return false;
+        }
+        if(!rough_eq(pos.z(), endPos.z)){
+          cout << "Z values did not match: " << pos << " " << endPos << endl;
+          return false;
+        }
+        return true;
+       }
+       cout << "Failed to calculate FK" << endl;
+       return false;
     }
     
     public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
@@ -159,32 +229,11 @@ namespace gazebo {
       boost::variate_generator<boost::mt19937&, boost::uniform_real<float> > gen(rng, u);
 
       // Construct the kinematic chains
-      vector<double> q;
-      Chain leftArm = constructChain(model->GetJoint("left_shoulder"), model->GetLink("left_hand"), q);
-      
-      // TODO: Move to separate function
-      ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(leftArm);
-      
-      // Create joint array
-      unsigned int nj = leftArm.getNrOfJoints();
-      assert(nj == q.size());
-      
-      KDL::JntArray jointPositions = JntArray(nj);
-      for(unsigned int i = 0; i < nj; i++){
-        jointPositions(i) = q[i];
-      }
 
-      // Create the frame that will contain the results
-      KDL::Frame cartPos;
-      
-      // Calculate forward position kinematics
-      bool status = fksolver.JntToCart(jointPositions, cartPos);
-      if(status >= 0){
-        std::cout << cartPos.p <<std::endl;
-        printf("%s \n","Succes, thanks KDL!");
-       } else {
-        printf("%s \n","Error: could not calculate forward kinematics :(");
-       }
+      physics::LinkPtr leftHand = model->GetLink("left_hand");
+      physics::JointPtr leftShoulder = model->GetJoint("left_shoulder");
+      Chain leftArm = constructChain(leftShoulder, leftHand);
+      assert(checkFK(leftArm, leftHand, leftShoulder));
     
       // TODO: Remaining three chains
       // TODO: Solve IK and set joint angles
