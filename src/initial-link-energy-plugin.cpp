b23e9ee035eb8cdbe6bc26b8d381898be010679a
Joshua Lurz <jlurz24@gmail.com>
2015-05-31 13:34:16 -0400

    Add a couple more measurement plugins

--- src/initial-link-energy-plugin.cpp
+++ src/initial-link-energy-plugin.cpp
@@ -40,29 +40,22 @@ namespace gazebo {
   static const string velocityDims[] = { "x", "y", "z", "r", "p", "y"};
 
 
-  class InitialLinkPositionPlugin : public ModelPlugin {
+  class InitialLinkEnergyPlugin : public ModelPlugin {
 
    private: physics::WorldPtr world;
    private: physics::ModelPtr model;
    private: event::ConnectionPtr connection;
    private:  ofstream csvFile;
 
-    public: InitialLinkPositionPlugin() : ModelPlugin() {
+    public: InitialLinkEnergyPlugin() : ModelPlugin() {
       #if(PRINT_DEBUG)
-      cout << "Constructing the initial link position plugin" << endl;
+      cout << "Constructing the initial link energy plugin" << endl;
       #endif
     }
 
     private: void writeHeader(ofstream& file){
        for (unsigned int i = 0; i < boost::size(links); ++i) {
-          for (unsigned int j = 0; j < boost::size(dims); ++j) {
-             file << links[i] << "(" << dims[j] << "),";
-          }
-       }
-       for (unsigned int i = 0; i < boost::size(links); ++i) {
-          for (unsigned int j = 0; j < boost::size(velocityDims); ++j) {
-             file << links[i] << "(" << velocityDims[j] << ") (m/s),";
-          }
+         file << links[i] << "(J),";
        }
       file << endl;
     }
@@ -72,7 +65,7 @@ namespace gazebo {
       model = _model;
 
       #if(PRINT_DEBUG)
-      cout << "Loading the initial link position plugin" << endl;
+      cout << "Loading the initial link energy plugin" << endl;
       #endif
       // Get the name of the folder to store the result in
       const char* resultsFolder = getenv("RESULTS_FOLDER");
@@ -82,7 +75,7 @@ namespace gazebo {
       }
       
       // Open a file to store the random values
-      const string resultsFileName = string(resultsFolder) + "/" + "link_positions.csv";
+      const string resultsFileName = string(resultsFolder) + "/" + "link_energy.csv";
       bool exists = boost::filesystem::exists(resultsFileName);
       csvFile.open(resultsFileName, ios::out | ios::app);
       assert(csvFile.is_open());
@@ -91,35 +84,27 @@ namespace gazebo {
         writeHeader(csvFile);
       }
 
-      for (unsigned int i = 0; i < boost::size(links); ++i) {
-         physics::LinkPtr link = _model->GetLink(links[i]);
-         csvFile << link->GetWorldPose().pos.x << ", " << link->GetWorldPose().pos.y << ", " << link->GetWorldPose().pos.z << ",";
-      }
-
-      connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&InitialLinkPositionPlugin::worldUpdate, this));
+      connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&InitialLinkEnergyPlugin::worldUpdate, this));
     }
 
     private: void worldUpdate(){
        #if(PRINT_DEBUG)
        cout << "Received world update for iteration: " << world->GetIterations() << endl;
        #endif
-       // Link velocity data not available until iteration 2.
+       // Link energy data not available until iteration 2.
        if (world->GetIterations() != 2){
           return;
        }
 
-      // Write out velocities
+      // Write out energies
        for (unsigned int i = 0; i < boost::size(links); ++i) {
           physics::LinkPtr link = model->GetLink(links[i]);
-          #if(PRINT_DEBUG)
-          cout << "Writing out link: " << links[i] << " with linear velocity: " << link->GetWorldLinearVel() << " and angular velocity: " << link->GetWorldAngularVel() << endl;
-          #endif
-          csvFile << link->GetWorldLinearVel().x << ", " << link->GetWorldLinearVel().y << ", " << link->GetWorldLinearVel().z << "," << link->GetWorldAngularVel().x << ", " << link->GetWorldAngularVel().y << ", " << link->GetWorldAngularVel().z << ",";
+          csvFile << link->GetWorldEnergy() << ",";
        }
        csvFile << endl;
        csvFile.close();
        event::Events::DisconnectWorldUpdateBegin(this->connection);
     }
   };
-  GZ_REGISTER_MODEL_PLUGIN(InitialLinkPositionPlugin)
+  GZ_REGISTER_MODEL_PLUGIN(InitialLinkEnergyPlugin)
 }
---
+++ src/link-energy-over-time-plugin.cpp
@@ -0,0 +1,116 @@
+#include <gazebo/gazebo.hh>
+#include <gazebo/math/Vector3.hh>
+#include <gazebo/physics/world.hh>
+#include <gazebo/sensors/ContactSensor.hh>
+#include <gazebo/physics/model.hh>
+#include <gazebo/sensors/sensors.hh>
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define PRINT_SENSORS 0
+#define PRINT_DEBUG 0
+
+using namespace std;
+
+namespace gazebo {
+
+   static const std::string links[] = {
+      "trunk",
+      "right_foot",
+      "left_foot",
+      "right_leg",
+      "left_leg",
+      "right_thigh",
+      "left_thigh",
+      "transpelvic_link",
+      "clavicular_link",
+      "head_neck",
+      "right_upper_arm",
+      "left_upper_arm",
+      "right_forearm",
+      "left_forearm",
+      "right_hand",
+      "left_hand"
+   };
+
+   static const double MAX_TIME = 2.0;
+    
+    class LinkEnergyOverTimePlugin : public ModelPlugin {
+
+    private: gazebo::transport::NodePtr node;
+    private: event::ConnectionPtr connection;
+    private: physics::WorldPtr world;
+    private: physics::ModelPtr model;
+    private: double averageLinkEnergy;
+    private: ofstream outputCSV;
+
+    public: LinkEnergyOverTimePlugin() : ModelPlugin() {
+        #if(PRINT_DEBUG)
+        cout << "Constructing the link energy over time plugin" << std::endl;
+        #endif
+    }
+        
+    public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
+      world = _model->GetWorld();
+      model = _model;
+
+      #if(PRINT_DEBUG)
+      cout << "Loading the link energy over time plugin" << endl;
+      #endif
+      connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&LinkEnergyOverTimePlugin::worldUpdate, this));
+
+      // Get the name of the folder to store the result in
+      const char* resultsFolder = std::getenv("RESULTS_FOLDER");
+      if(resultsFolder == nullptr){
+         cout << "Results folder not set. Using current directory." << endl;
+         resultsFolder = "./";
+      }
+
+      const string resultsFileName = string(resultsFolder) + "/" + "link_energy_over_time.csv";
+      outputCSV.open(resultsFileName, ios::out);
+      assert(outputCSV.is_open());
+      writeHeader(outputCSV);
+
+      double linkEnergy = 0;
+      for (unsigned int i = 0; i < boost::size(links); ++i) {
+         physics::LinkPtr link = model->GetLink(links[i]);
+         linkEnergy += link->GetWorldEnergy();
+      }
+      outputCSV << world->GetSimTime().Float() << ", " << linkEnergy << endl;
+    }
+    
+    private: void writeHeader(ofstream& outputCSV){
+       outputCSV << "Time (s), Total Energy (J)" << endl;
+    }
+
+    private: void worldUpdate(){
+#if(PRINT_DEBUG)
+       cout << "Updating the world for time: " << world->GetSimTime().Float() << endl;
+#endif
+       for (unsigned int i = 0; i < boost::size(links); ++i) {
+          const physics::LinkPtr link = model->GetLink(links[i]);
+          averageLinkEnergy += link->GetWorldEnergy();
+       }
+
+       if (world->GetSimTime().nsec % (10 * 1000000) == 0) {
+          outputCSV << world->GetSimTime().Float() << ", " << averageLinkEnergy / 10.0 << endl;
+          averageLinkEnergy = 0;
+       }
+
+       if(world->GetSimTime().Float() >= MAX_TIME){
+          #if(PRINT_DEBUG)
+          cout << "Scenario completed. Updating results" << endl;
+          #endif
+          event::Events::DisconnectWorldUpdateBegin(this->connection);
+
+          outputCSV << endl;
+          outputCSV.close();
+       }
+    }
+    };
+    GZ_REGISTER_MODEL_PLUGIN(LinkEnergyOverTimePlugin);
+}
+
